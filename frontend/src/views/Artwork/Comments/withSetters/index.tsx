cdd67db3eae6277988c92893c0424f0675e53851
nicolas lp <nicolas_lepasteur@live.fr>
2021-12-07 01:11:00 +0100

    continue Artwork (Comments setters)

--- frontend/src/views/Artwork/Comments/withSetters/index.tsx
+++ frontend/src/views/Artwork/Comments/withSetters/index.tsx
@@ -8,30 +8,57 @@ import type { InjectedProps as PropsFromWithComments } from '../withComments';
 // possibilité de commenter / reply
 // possibilité d'update un comment / reply
 // possibilité de remove un comment / reply
-const like =
-  (dispatch: PropsFromWithComments['dispatchLocally']) =>
-  async ({
-    page,
-    type,
-    votable_id,
-    votable_type,
-  }: {
-    page: number;
-    type: 'like' | ' comment';
-    votable_type: 'like' | ' comment';
+
+// décider d'après comment.liked en amont si sera utilisé comme delete ou comme post
+// expliquer pourquoi ce choix (ne voulait pas ajouter responsabilité à fonction "vote" de déterminer ça,
+// aurait été trop de responsabilités)
+type Vote = {
+  created_at: string;
+  id: string;
+  user_id: string;
+  votable_id: string;
+  votable_type: 'comment' | 'like';
+};
+
+function vote<
+  RequestBody = {
+    type: 'like' | 'comment';
     votable_id: string;
-  }) => {
-    console.log('like triggered with id: ', votable_id);
+    votable_type: 'like' | 'comment';
+  }
+  // éventuellement expliquer pourquoi avoir choisi de créer type dans generic plutôt que dans alias type
+  // (pas réutilisé et parce que propre à une fonction bien spécifique)
+>(
+  succesCb: (votableId: string) => void,
+  // expliquer démarche d'entre 2 de sorte à assouplir typage de cbs (pas de typage avec Dispatch<GenericFetchState<Comment> | etc...>) tout en maintenant un contrat
+  // expliquer aussi pourquoi fait en internet et pas d'après value retournée issue de fetch (car response.ok détermine est
+  // aurait été étrange à retourner)
+  failureCb: (errorMessage: string) => void
+) {
+  return async (body: RequestBody) => {
     try {
-      // http post request
-      // pas de relance de fetch après mais juste une mise à jour du state
-      // correspondant à changement si réponse ok
-      // const response = await fetch('', {}) <= si réponse alors signifie que a été pris en compte
-      dispatch({ type: 'UPDATE_LIKES', payload: votable_id });
+      const response = await fetch('/votes');
+      const data = await response.json();
+      if (!response.ok) {
+        throw data.message;
+      }
+      succesCb(data as Vote['votable_id']);
     } catch (e) {
-      // handle error
+      failureCb(e as string);
     }
   };
+}
+// on click déclenche selon liked (donc déterminer en dehors si DELETE ou POST, autrement serait trop à gérer pour une seule fonction)
+// const vote = (body: {
+//   page: number;
+//   type: 'like' | 'comment';
+//   votable_id: string;
+//   votable_type: 'like' | 'comment';
+// }) => {
+//   return new Promise((resolve, reject) => {
+//     resolve(fetch(''));
+//   }).then((response) => response.json());
+// };
 
 export type InjectedProps = Omit<PropsFromWithComments, 'dispatchLocally'> & {
   like: ReturnType<typeof like>;
@@ -42,7 +69,7 @@ const withSetters = (UnwrappedComment: ComponentType<InjectedProps>) => {
     dispatchLocally,
     ...props
   }: PropsFromWithComments) => {
-    return <UnwrappedComment like={like(dispatchLocally)} {...props} />;
+    return <UnwrappedComment like={like2(dispatchLocally)} {...props} />;
   };
   return WithSetters;
 };
