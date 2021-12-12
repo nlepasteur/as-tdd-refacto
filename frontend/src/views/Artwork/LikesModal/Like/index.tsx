e0d97af92ef85be9af7b807f803c9b5b9e902b5a
nicolas lp <nicolas_lepasteur@live.fr>
2021-12-13 00:23:55 +0100

    add LikesModal open/close animation

--- frontend/src/views/Artwork/LikesModal/Like/index.tsx
+++ frontend/src/views/Artwork/LikesModal/Like/index.tsx
@@ -4,9 +4,12 @@ import type { Vote } from '@types';
 // libs
 import { Link } from 'react-router-dom';
 
-type InjectedProps = { like: Vote };
+type InjectedProps = { like: Vote; handleCloseLikesModal(): void };
 
-const Like: ComponentType<InjectedProps> = ({ like }) => (
+const Like: ComponentType<InjectedProps> = ({
+  like,
+  handleCloseLikesModal,
+}) => (
   <>
     <Link to={`/${like.user.username}`}>
       <img src={like.user.small_cover_url} alt={''} />
@@ -14,6 +17,9 @@ const Like: ComponentType<InjectedProps> = ({ like }) => (
     <Link to={`/${like.user.username}`}>{like.user.username}</Link>
     <div>{like.user.followers_count}</div>
     <div>{like.user.projects_count}</div>
+    <button onClick={handleCloseLikesModal} style={{ color: 'red' }}>
+      close
+    </button>
     <button>follow</button>
   </>
 );
--- frontend/src/views/Artwork/LikesModal/index.tsx
+++ frontend/src/views/Artwork/LikesModal/index.tsx
@@ -2,7 +2,8 @@
 import type { ComponentType } from 'react';
 import type { InjectedProps as PropsFromWithLikes } from './withLikes';
 // libs
-import { useRef } from 'react';
+import { useRef, useLayoutEffect } from 'react';
+import gsap from 'gsap';
 // hooks
 import useOnClickOutside from 'hooks/useOnClickOutside';
 // components
@@ -13,15 +14,50 @@ type InjectedProps = PropsFromWithLikes;
 
 const Likes: ComponentType<InjectedProps> = ({ likes, toggleLikesModal }) => {
   const modal = useRef(null);
-  useOnClickOutside(modal, () => toggleLikesModal());
+  const timeline = useRef<gsap.core.Timeline | null>(null);
+  const handleCloseLikesModal = () => {
+    if (timeline.current) {
+      timeline.current.reverse().then(() => toggleLikesModal());
+    }
+  };
+
+  useOnClickOutside(modal, handleCloseLikesModal);
+  useLayoutEffect(() => {
+    timeline.current = gsap
+      .timeline()
+      .set(modal.current, { y: '-100%', opacity: 0 })
+      .to(modal.current, { y: '-50%', top: '50%', duration: 0.5, opacity: 1 });
+  }, []);
+
   return (
-    <ul ref={modal}>
-      {likes.map((like) => (
-        <li key={like.id} style={{ color: 'white' }}>
-          <Like like={like} />
-        </li>
-      ))}
-    </ul>
+    <div
+      style={{
+        backgroundColor: 'rgba(0,0,0,.5)',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        width: '100vw',
+        height: '100vh',
+      }}
+    >
+      <ul
+        ref={modal}
+        style={{
+          position: 'absolute',
+          backgroundColor: '#171717',
+          width: '300px',
+          height: '150px',
+          left: '50%',
+          transform: 'translateX(-50%)',
+        }}
+      >
+        {likes.map((like) => (
+          <li key={like.id} style={{ color: 'white' }}>
+            <Like like={like} handleCloseLikesModal={handleCloseLikesModal} />
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 };
 
